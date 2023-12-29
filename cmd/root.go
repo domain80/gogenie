689ed380888bd3308061a37d3d172d32f64a173e
domain80 <work.davidmainoo@gmail.com>
2023-12-29 02:32:29 +0000

    Define halp text and about text of the cli

--- cmd/root.go
+++ cmd/root.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,27 +16,39 @@ limitations under the License.
 package cmd
 
 import (
+	"embed"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "genie",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "A cli tool to quickly generate boilerplate code for developing webapps in golang ",
+	Long: `
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+    /$$$$$$                      /$$          
+   /$$__  $$                    |__/          
+  | $$  \__/  /$$$$$$  /$$$$$$$  /$$  /$$$$$$ 
+  | $$ /$$$$ /$$__  $$| $$__  $$| $$ /$$__  $$
+  | $$|_  $$| $$$$$$$$| $$  \ $$| $$| $$$$$$$$
+  | $$  \ $$| $$_____/| $$  | $$| $$| $$_____/
+  |  $$$$$$/|  $$$$$$$| $$  | $$| $$|  $$$$$$$
+   \______/  \_______/|__/  |__/|__/ \_______/
+
+  A cli tool to quickly generate boilerplate code for developing 
+  webapps in golang. The tool can be used as a standalone tool or 
+  together with the go generate command to generate various parts 
+  of the application such as models, routers etc`,
+
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
 }
+
+//go:embed assets
+var assets embed.FS
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
@@ -58,5 +70,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
